1ba6d3bd0984481b656bbd95e813810fca191d08
Andres Yaruro <andresyaruro20@gmail.com>
2026-03-19 17:44:47 -0500

    cambios en producto cotizable. validacion para que exista solo uno.

--- app/Application/Validations/ImagenProductoValidation.php
+++ app/Application/Validations/ImagenProductoValidation.php
@@ -8,23 +8,22 @@ use App\Domain\Entity\RespuestaEntity;
 
 class ImagenProductoValidation
 {
-
     private static array $translations = [
-    "es" => [
-        "validation_success" => "Validación correcta",
-        "validation_error" => "Errores de validación",
-        "product_id_required" => "El id del producto es obligatorio",
-        "owner_invalid" => "El owner no es dueño del producto",
-        "image_required" => "Debe enviar al menos una imagen"
-    ],
-    "en" => [
-        "validation_success" => "Validation successful",
-        "validation_error" => "Validation errors",
-        "product_id_required" => "Product id is required",
-        "owner_invalid" => "The owner does not own the product",
-        "image_required" => "You must send at least one image"
-    ]
-];
+        'es' => [
+            'validation_success' => 'Validación correcta',
+            'validation_error' => 'Errores de validación',
+            'product_id_required' => 'El id del producto es obligatorio',
+            'owner_invalid' => 'El owner no es dueño del producto',
+            'image_required' => 'La imagen del producto es requerida',
+        ],
+        'en' => [
+            'validation_success' => 'Validation successful',
+            'validation_error' => 'Validation errors',
+            'product_id_required' => 'Product id is required',
+            'owner_invalid' => 'The owner does not own the product',
+            'image_required' => 'Product image is required',
+        ],
+    ];
 
     public static function validar(ImagenProductoInputDto $dto, ProductoService $productoService, string $lang, int $ownerId): RespuestaEntity
     {
@@ -43,19 +42,22 @@ class ImagenProductoValidation
     private static function validarIdProducto(?int $valor, array &$errores, ProductoService $productoService, string $lang, int $ownerId): void
     {
         if (empty($valor) || $valor <= 0) {
-            $errores["idProducto"] = self::$translations[$lang]['product_id_required'];
+            $errores['idProducto'] = self::$translations[$lang]['product_id_required'];
+
             return;
 
         }
 
         $resp = $productoService->GetById($valor, $lang);
-        if (!$resp->IsSuccess) {
-            $errores["producto"] = $resp->Message;
+        if (! $resp->IsSuccess) {
+            $errores['producto'] = $resp->Message;
+
             return;
         }
 
-        if($resp->Data->IdOwner != $ownerId){
-            $errores["producto"] = self::$translations[$lang]['owner_invalid'];
+        if ($resp->Data->IdOwner != $ownerId) {
+            $errores['producto'] = self::$translations[$lang]['owner_invalid'];
+
             return;
         }
 
@@ -64,7 +66,7 @@ class ImagenProductoValidation
     private static function validarImagen(?string $valor, array &$errores, $lang): void
     {
         if (empty($valor)) {
-            $errores["imagen"] = self::$translations[$lang]['image_required'];;
+            $errores['imagen'] = self::$translations[$lang]['image_required'];
         }
     }
 }
--- app/Application/Validations/ProductoCotizableValidation.php
+++ app/Application/Validations/ProductoCotizableValidation.php
@@ -4,40 +4,40 @@ namespace App\Application\Validations;
 
 use App\Application\DTOs\Producto\ProductoCotizableDto;
 use App\Application\Services\ProductoService;
+use App\Application\Services\SkuService;
 use App\Application\Services\TipoProductoService;
 use App\Application\Services\TipoSetupService;
 use App\Domain\Entity\RespuestaEntity;
 
 class ProductoCotizableValidation
 {
-
     private static array $translations = [
-        "es" => [
-            "validation_success" => "Validación correcta",
-            "validation_error" => "Errores de validación",
-            "validation_idproducto" => "El id del producto es obligatorio",
-            "validation_id_tiposetup" => "El Id del setup es obligatorio",
-            "owner_invalid" => "El owner no es dueño del producto",
-            "type_product_invalid" => "El tipo de producto no es valido",
-
+        'es' => [
+            'validation_success' => 'Validación correcta',
+            'validation_error' => 'Errores de validación',
+            'validation_idproducto' => 'El id del producto es obligatorio',
+            'validation_id_tiposetup' => 'El Id del setup es obligatorio',
+            'owner_invalid' => 'El owner no es dueño del producto',
+            'type_product_invalid' => 'El tipo de producto no es valido',
+            'exist_product' => 'Ya existe el producto cotizable',
         ],
-        "en" => [
-            "validation_success" => "Validation successful",
-            "validation_error" => "Validation errors",
-            "validation_idproducto" => "Product id is required",
-            "validation_id_tiposetup" => "Setup id is required",
-             "owner_invalid" => "The owner does not own the product",
-             "type_product_invalid" => "Type product is invalid",
-        ]
+        'en' => [
+            'validation_success' => 'Validation successful',
+            'validation_error' => 'Validation errors',
+            'validation_idproducto' => 'Product id is required',
+            'validation_id_tiposetup' => 'Setup id is required',
+            'owner_invalid' => 'The owner does not own the product',
+            'type_product_invalid' => 'The product type is invalid',
+            'exist_product' => 'The quotable product already exists',
+        ],
     ];
 
-    public static function validar(ProductoCotizableDto $dto, ProductoService $productoService, TipoSetupService $tipoSetupService, string $lang, int $ownerId, TipoProductoService $tipoProductoService): RespuestaEntity
+    public static function validar(ProductoCotizableDto $dto, ProductoService $productoService, TipoSetupService $tipoSetupService, string $lang, int $ownerId, TipoProductoService $tipoProductoService, SkuService $skuService): RespuestaEntity
     {
         $errores = [];
 
-        self::validarProducto($dto->IdProducto, $errores, $productoService, $lang, $ownerId, $tipoProductoService);
+        self::validarProducto($dto->IdProducto, $errores, $productoService, $lang, $ownerId, $tipoProductoService, $skuService);
         self::validarTipoSetup($dto->IdTipoSetup, $errores, $tipoSetupService, $lang);
-
 
         return new RespuestaEntity(
             empty($errores) ? self::$translations[$lang]['validation_success'] : self::$translations[$lang]['validation_error'],
@@ -46,34 +46,45 @@ class ProductoCotizableValidation
         );
     }
 
-
-    private static function validarProducto(?int $valor, array &$errores, ProductoService $productoService, string $lang, int $ownerId, TipoProductoService $tipoProductoService): void
+    private static function validarProducto(?int $valor, array &$errores, ProductoService $productoService, string $lang, int $ownerId, TipoProductoService $tipoProductoService, SkuService $skuService): void
     {
         if (empty($valor) || $valor <= 0) {
-            $errores["idProducto"] = self::$translations[$lang]['validation_idproducto'];
-            return; 
+            $errores['idProducto'] = self::$translations[$lang]['validation_idproducto'];
+
+            return;
         }
 
         $resp = $productoService->GetById($valor, $lang);
-        if (!$resp->IsSuccess) {
-            $errores["producto"] = $resp->Message;
+        if (! $resp->IsSuccess) {
+            $errores['producto'] = $resp->Message;
+
             return;
         }
 
-        if($resp->Data->IdOwner != $ownerId){
-            $errores["producto"] = self::$translations[$lang]['owner_invalid'];
+        if ($resp->Data->IdOwner != $ownerId) {
+            $errores['producto'] = self::$translations[$lang]['owner_invalid'];
+
             return;
         }
 
-        $respTipoProducto = $tipoProductoService->GetByName("COTIZACION", $lang);
+        $respTipoProducto = $tipoProductoService->GetByName('COTIZACION', $lang);
 
-        if(!$respTipoProducto->IsSuccess){
-            $errores["producto"] = $respTipoProducto->Message;
+        if (! $respTipoProducto->IsSuccess) {
+            $errores['producto'] = $respTipoProducto->Message;
+
             return;
         }
 
-        if($respTipoProducto->Data->Id != $resp->Data->IdTipoProducto){
-            $errores["producto"] = self::$translations[$lang]['type_product_invalid'];
+        if ($respTipoProducto->Data->Id != $resp->Data->IdTipoProducto) {
+            $errores['producto'] = self::$translations[$lang]['type_product_invalid'];
+
+            return;
+        }
+
+        $existProducto = $skuService->GetByProducto($resp->Data->Id, $lang);
+        if ($existProducto->IsSuccess) {
+            $errores['producto'] = self::$translations[$lang]['exist_product'];
+
             return;
         }
     }
@@ -81,14 +92,15 @@ class ProductoCotizableValidation
     private static function validarTipoSetup(?int $valor, array &$errores, TipoSetupService $tipoSetupService, string $lang): void
     {
         if (empty($valor) || $valor <= 0) {
-            $errores["idTipoSetup"] = self::$translations[$lang]['validation_id_tiposetup'];
+            $errores['idTipoSetup'] = self::$translations[$lang]['validation_id_tiposetup'];
+
             return;
 
         }
 
         $resp = $tipoSetupService->GetById($valor, $lang);
-        if (!$resp->IsSuccess) {
-            $errores["tipoSetup"] = $resp->Message;
+        if (! $resp->IsSuccess) {
+            $errores['tipoSetup'] = $resp->Message;
         }
 
     }
