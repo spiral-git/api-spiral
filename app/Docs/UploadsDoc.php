cfd4ce6dfc6b8c37c9e6a719d8af1045e736faf2
Andres Yaruro <andresyaruro20@gmail.com>
2026-03-23 17:56:26 -0500

    resolviendo

--- app/Docs/UploadsDoc.php
+++ app/Docs/UploadsDoc.php
@@ -6,29 +6,55 @@ use OpenApi\Attributes as OA;
 
 class UploadsDoc
 {
-    // #[OA\Post(
-    //     path: '/uploads/upload',
-    //     tags: ['Uploads'],
-    //     summary: 'Subir imagen o pdf en formato: jpg,jpeg,png,webp,mp4,mov,avi,pdf',
-    //     required: ['dir', 'lang'],
-    //     parameters: [
-    //         new OA\Property(
-    //             property: 'dir',
-    //             type: 'string',
-    //             example: ''
-    //         ),
-    //         new OA\Property(
-    //             property: 'lang',
-    //             type: 'string',
-    //             example: ''
-    //         ),
-    //     ],
-    //     responses: [
-    //         new OA\Response(
-    //             response: 200,
-    //             description: 'Operación exitosa'
-    //         ),
-    //     ]
-    // )]
-    // public function created() {}
+    #[OA\Post(
+        path: '/uploads/upload',
+        tags: ['Uploads'],
+        summary: 'Subir imagen, video o pdf',
+        
+        parameters: [
+            new OA\Parameter(
+                name: 'dir',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'string'),
+                example: 'productos'
+            ),
+            new OA\Parameter(
+                name: 'lang',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'string'),
+                example: 'es'
+            ),
+        ],
+
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\MediaType(
+                mediaType: 'multipart/form-data',
+                schema: new OA\Schema(
+                    required: ['file'],
+                    properties: [
+                        new OA\Property(
+                            property: 'file',
+                            type: 'string',
+                            format: 'binary'
+                        )
+                    ]
+                )
+            )
+        ),
+
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: 'Archivo subido correctamente'
+            ),
+            new OA\Response(
+                response: 400,
+                description: 'Error en la solicitud'
+            )
+        ]
+    )]
+    public function upload() {}
 }
