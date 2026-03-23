b6341e363baaab411a7425e44491302e81a0629e
Andres Yaruro <andresyaruro20@gmail.com>
2026-03-23 18:38:10 -0500

    subiendo nuevamente

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -40,37 +40,46 @@ class UploadFileController extends BaseController
             $dir = $request->input('dir') ?? 'default';
             $lang = $request->input('lang') ?? 'es';
 
+            // 🔒 Sanitizar dir
+            $dir = preg_replace('/[^A-Za-z0-9_\-]/', '', $dir);
+            $dir = $dir ?: 'default';
+
             $resp = $this->validarTokenHeaderAll($lang);
 
             if (! $resp->IsSuccess) {
-                return response()->json(
-                    $resp,
-                    Response::HTTP_UNAUTHORIZED
-                );
+                return response()->json($resp, Response::HTTP_UNAUTHORIZED);
             }
 
             $request->validate([
                 'file' => 'required|file|mimes:jpg,jpeg,png,webp,mp4,mov,avi,pdf|max:51200',
             ]);
-            
+
             $file = $request->file('file');
 
-            $filename = time().'_'.uniqid().'.'.$file->getClientOriginalExtension();
+            // 🔥 nombre seguro
+            $filename = bin2hex(random_bytes(16)).'.'.$file->getClientOriginalExtension();
 
-            $path = $file->storeAs("uploads/$dir", $filename, 'public');
+            $destinationPath = public_path("uploads/$dir");
 
-            $url = asset('storage/'.$path);
+            if (! is_dir($destinationPath)) {
+                mkdir($destinationPath, 0775, true);
+            }
 
+            $file->move($destinationPath, $filename);
+
+            $url = asset("uploads/$dir/$filename");
+ 
             $respuesta = new RespuestaEntity(
                 $this->translations[$lang]['success'] ?? '',
                 true,
-                $url
+                [
+                    'url' => $url,
+                    'name' => $filename,
+                    'type' => $file->getClientMimeType(),
+                ]
             );
 
-            return response()->json(
-                $respuesta,
-                Response::HTTP_CREATED
-            );
+            return response()->json($respuesta, Response::HTTP_CREATED);
 
         } catch (Exception $e) {
 
@@ -80,10 +89,7 @@ class UploadFileController extends BaseController
                 null
             );
 
-            return response()->json(
-                $respuesta,
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
+            return response()->json($respuesta, Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
