28096769b1fe4d99f2e94de1fc0cd9d66d9282e4
Andres Yaruro <andresyaruro20@gmail.com>
2026-03-23 22:32:07 -0500

    otra prueba

--- app/Http/Controllers/ProductoPlanController.php
+++ app/Http/Controllers/ProductoPlanController.php
@@ -11,18 +11,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProductoPlanController extends BaseController
 {
-    private UsuarioService $_usuarioService;
 
-    private TipoUsuarioService $_tipoUsuarioService;
-
-    private ProductoPlanService $_service;
-
-    public function __construct(UsuarioService $usuarioService, TipoUsuarioService $tipoUsuarioService, ProductoPlanService $service)
+    public function __construct()
     {
-        $this->_usuarioService = $usuarioService;
-        $this->_tipoUsuarioService = $tipoUsuarioService;
-        $this->_service = $service;
-        parent::__construct($this->_usuarioService, $this->_tipoUsuarioService);
     }
 
 
